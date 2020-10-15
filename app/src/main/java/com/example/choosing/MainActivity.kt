1190ad18a721035e89192f8418bb6638d1826835
sanguinenigro <sanguinenigro@outlook.com>
2020-10-15 21:52:07 +0200

    kotlinv2

--- app/src/main/java/com/example/choosing/MainActivity.kt
+++ app/src/main/java/com/example/choosing/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         wchodze.setOnClickListener {
-            var message = Toast.makeText(applicationContext,"Dziękuję!",Toast.LENGTH_LONG)
+            var message = Toast.makeText(applicationContext,"Dziękuję!",Toast.LENGTH_SHORT)
             message.show()
         }
 
