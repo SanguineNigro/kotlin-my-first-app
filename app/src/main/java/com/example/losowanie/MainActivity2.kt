8e01ee457e35715d6579d4e55fbe9250f15afd64
sanguinenigro <sanguinenigro@outlook.com>
2020-10-17 00:23:42 +0200

    proba-losowania1

--- app/src/main/java/com/example/losowanie/MainActivity2.kt
+++ app/src/main/java/com/example/losowanie/MainActivity2.kt
@@ -3,6 +3,7 @@ package com.example.losowanie
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main2.*
+import kotlin.random.Random
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +11,24 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         losuj.setOnClickListener {
+            fun main() {
+                val diceRange = 1..6
+                val randomString = diceRange.random()
+                println("Random number: ${randomString}")
+            }
+            fun main() {
+                val myFirstDice = Dice ()
+                println(myFirstDice.sides)
+                myFirstDice.roll()
+            }
 
-        }
+            class Dice {
+                var sides = 6
 
+                fun roll() {
+                    val randomString = (1..6).random()
+                    println(randomString)
+                }
+            }
     }
 }
