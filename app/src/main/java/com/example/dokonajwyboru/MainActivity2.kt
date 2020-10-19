2ca9c9c70a293cbc59cd4c0a8c8d483df0c81424
sanguinenigro <sanguinenigro@outlook.com>
2020-10-19 22:07:47 +0200

    click-on

--- app/src/main/java/com/example/dokonajwyboru/MainActivity2.kt
+++ app/src/main/java/com/example/dokonajwyboru/MainActivity2.kt
@@ -2,10 +2,17 @@ package com.example.dokonajwyboru
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        losuje.setOnClickListener {
+
+        }
+
     }
 }
