fc8f36e95bc3bf68c7cbd0d4da7475df8b2bc3aa
sanguinenigro <sanguinenigro@outlook.com>
2020-10-19 20:36:45 +0200

    android-decyduje

---
+++ app/src/main/java/com/example/dokonajwyboru/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.dokonajwyboru
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        wchodze.setOnClickListener {
+            var message = Toast.makeText(applicationContext,"Dziękuję",Toast.LENGTH_SHORT)
+            message.show()
+
+            var newActivity: Intent = Intent (applicationContext, MainActivity2::class.java)
+            startActivity(newActivity)
+        }
+
+        rezygnuje.setOnClickListener {
+            var message = Toast.makeText(applicationContext,"Może innym razem?",Toast.LENGTH_SHORT)
+            message.show()
+    }
+}
