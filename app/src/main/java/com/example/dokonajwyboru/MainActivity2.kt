07e942401e6062d0c2c1d05bf642c1aeedd6a8d9
sanguinenigro <sanguinenigro@outlook.com>
2020-10-19 23:31:30 +0200

    val-click

--- app/src/main/java/com/example/dokonajwyboru/MainActivity2.kt
+++ app/src/main/java/com/example/dokonajwyboru/MainActivity2.kt
@@ -7,9 +7,17 @@ import kotlin.random.Random
 
 class MainActivity2 : AppCompatActivity() {
 
+    val decideList = arrayListOf(dodajemy)
+
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+            losuje.setOnClickListener {
+                val random = Random
+                val randomDecide = random.nextBoolean(decideList.count())
+
+            }
 
     }
 }
